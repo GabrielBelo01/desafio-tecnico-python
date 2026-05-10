b80330e6e0b49c31ab3f399c696966cc039f1bf4
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2026-05-10 16:49:49 -0300

    projeto-02: models - tabela consultas

---
+++ projeto-02/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Consulta(Base):
+    __tablename__ = "consultas"
+
+    id          = Column(Integer, primary_key=True, autoincrement=True)
+    wallet      = Column(String, nullable=False)
+    token       = Column(String, nullable=False)
+    saldo       = Column(Float, nullable=False)
+    coletado_em = Column(DateTime, default=datetime.utcnow)
