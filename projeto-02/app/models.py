589511690c9c2d72d7489446054b08bb8acc20d6
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2026-05-11 10:20:00 -0300

    projeto-02: models - timestamp em horario de Brasilia

--- projeto-02/app/models.py
+++ projeto-02/app/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import declarative_base
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
+BRT = timezone(timedelta(hours=-3))
 
 Base = declarative_base()
 
@@ -11,4 +13,4 @@ class Consulta(Base):
     wallet      = Column(String, nullable=False)
     token       = Column(String, nullable=False)
     saldo       = Column(Float, nullable=False)
-    coletado_em = Column(DateTime, default=datetime.utcnow)
+    coletado_em = Column(DateTime, default=lambda: datetime.now(BRT))
